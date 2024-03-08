0ab6e53753d91b2c9271a0b319e21ee5acb046bc
mariana <marianass712@gmail.com>
2024-03-08 18:26:33 -0300

    agregamos los componentes comunes Footer y menu, luego el componente Inicio en la carpeta pages

---
+++ src/components/commons/Menu.jsx
@@ -0,0 +1,25 @@
+import {Navbar, Container, Nav} from 'react-bootstrap'
+import logo from '../../assets/Coffee_Logo.png'
+
+const Menu = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img src={logo} alt="logo Rolling Coffee" className='img-fluid' width={150}/>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Inicio</Nav.Link>
+              <Nav.Link href="#link">Administrador</Nav.Link>
+              <Nav.Link href="#link">Login</Nav.Link>
+              <Nav.Link href="#link">Registro</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+};
+
+export default Menu;
