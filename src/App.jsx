ed3d86f8d9e038ab1aa25d49e034edc886fd8493
mariana <marianass712@gmail.com>
2024-03-08 19:46:05 -0300

    agregamos las rutas en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,36 @@ import Footer from './components/common/Footer'
 import Inicio from './components/pages/Inicio'
 import Administrador from './components/pages/Administrador'
 import Error404 from './components/pages/Error404';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
  
   return (
-    <>
-      <Menu></Menu>
-      {/* <Inicio></Inicio> */}
-      <Administrador></Administrador>
-      {/* <Error404></Error404>  */}
-      <Footer></Footer>
-    </>
-  )
+    <BrowserRouter>
+    <Menu></Menu>
+    {/* <Menu usuarioLogueado = {usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Menu> */}
+    <Routes>
+      <Route exact path="/" element={<Inicio></Inicio>}></Route>
+      {/* <Route
+        exact
+        path="/detalleProducto/:id"
+        element={<DetalleProducto></DetalleProducto>}
+      ></Route> */}
+      {/* <Route exact path="/login" element={<Login setUsuarioLogueado = {setUsuarioLogueado}></Login>}></Route> */}
+      {/* <Route
+        exact
+        path="/administrador/*"
+        element={
+          <RutasProtegidas>
+            <RutasAdmin></RutasAdmin>
+          </RutasProtegidas>
+        }
+      ></Route> */}
+      <Route path="/administrador" element={<Administrador></Administrador>}></Route>
+      <Route path="/error404" element={<Error404></Error404>}></Route>
+    </Routes>
+    <Footer></Footer>
+  </BrowserRouter>
+  );
 }
 
 export default App
