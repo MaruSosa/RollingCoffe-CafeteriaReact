e5bba6453b0d541f4c660863524f95493e10e7e3
mariana <marianass712@gmail.com>
2024-03-08 21:09:08 -0300

    traemos los productos de la api en el montaje de administrador

--- src/components/pages/Administrador.jsx
+++ src/components/pages/Administrador.jsx
@@ -1,8 +1,24 @@
 import { Button, Table } from "react-bootstrap";
 import ItemProducto from "./producto/ItemProducto";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { leerProductosAPI } from "../../helpers/queries";
+
 const Administrador = () => {
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    traerProductos();
+  }, []);
+  
+const traerProductos = async()=>{
+    try {
+       const listaProductosAPI = await leerProductosAPI();
+       setProductos(listaProductosAPI);
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <section className="container mainSection">
       <div className="d-flex justify-content-between align-items-center mt-5">
