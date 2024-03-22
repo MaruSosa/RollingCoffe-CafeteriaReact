e675052f4d8821c47fb23b569d913eb365640f58
mariana <marianass712@gmail.com>
2024-03-22 18:48:59 -0300

    se realizo la logica, rutas para traer la informacion del producto que se mostrara en detalleProducto

--- src/App.jsx
+++ src/App.jsx
@@ -16,27 +16,13 @@ function App() {
     <Menu></Menu>
     {/* <Menu usuarioLogueado = {usuarioLogueado} setUsuarioLogueado={setUsuarioLogueado}></Menu> */}
     <Routes>
-      <Route exact path="/" element={<Inicio></Inicio>}></Route>
-      {/* <Route
-        exact
-        path="/detalleProducto/:id"
-        element={<DetalleProducto></DetalleProducto>}
-      ></Route> */}
-      {/* <Route exact path="/login" element={<Login setUsuarioLogueado = {setUsuarioLogueado}></Login>}></Route> */}
-      {/* <Route
-        exact
-        path="/administrador/*"
-        element={
-          <RutasProtegidas>
-            <RutasAdmin></RutasAdmin>
-          </RutasProtegidas>
-        }
-      ></Route> */}
-      <Route path="/administrador" element={<Administrador></Administrador>}></Route>
-      <Route exact path="/detalleProducto" element={<DetalleProducto></DetalleProducto>}></Route>
-      <Route exact path="/administrador/crear" element={<FormularioProducto  editar={false} titulo='Nuevo producto'></FormularioProducto>}></Route>
+    <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/detalleProducto/:id" element={<DetalleProducto></DetalleProducto>}></Route>
+        {/* <Route exact path="/login" element={<Login></Login>}></Route> */}
+        <Route exact path="/administrador" element={<Administrador></Administrador>}></Route>
+        <Route exact path="/administrador/crear" element={<FormularioProducto editar={false} titulo='Nuevo producto'></FormularioProducto>}></Route>
         <Route exact path="/administrador/editar/:id" element={<FormularioProducto editar={true} titulo='Editar producto'></FormularioProducto>}></Route>
-      <Route path="/error404" element={<Error404></Error404>}></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
     </Routes>
     <Footer></Footer>
   </BrowserRouter>
--- src/components/pages/DetalleProducto.jsx
+++ src/components/pages/DetalleProducto.jsx
@@ -1,6 +1,36 @@
 import { Container, Card, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { obtenerProductoAPI } from "../../helpers/queries";
+import Swal from "sweetalert2";
+
 const DetalleProducto = () => {
+  const { id } = useParams(); 
+  /*useParams es un hook proporcionado por React Router que te permite acceder a los parámetros de la URL en un componente funcional. Esto es útil cuando estás utilizando React Router para manejar la navegación en tu aplicación y necesitas acceder a los parámetros que se pasan en la URL, como identificadores únicos u otra información dinámica. */
+  const [producto, setProducto] = useState({});
+
+  useEffect(() => {
+    //useEffect es un hook de React que te permite realizar efectos secundarios en componentes funcionales. Estos efectos secundarios pueden ser cosas como suscribirse a recursos externos (como datos de una API), manipular el DOM directamente o ejecutar código después de que se haya renderizado un componente.
+    //buscar el producto que quiero maquetar
+    cargarDetalle();
+  }, []);
+
+  const cargarDetalle = async () => {
+    /*La palabra clave await es parte de la sintaxis de JavaScript y se utiliza en funciones que tienen la palabra clave async. Su función es pausar la ejecución de una función asíncrona hasta que una promesa se resuelva y devolver el resultado resuelto.En esencia, cuando utilizas await, estás diciendo al motor de JavaScript que espere hasta que la promesa se resuelva y devuelva el valor resuelto antes de continuar ejecutando el código siguiente.  */
+    const respuesta = await obtenerProductoAPI(id); //trae el id de queries
+    if (respuesta.status === 200) {
+      //mostrar el producto en la card
+      const datoProducto = await respuesta.json();
+      setProducto(datoProducto);
+    } else {
+      Swal.fire({
+        title: "Ocurrio un error",
+        text: "Intente realizar esta operacion en unos minutos",
+        icon: "error",
+      });
+    }
+  };
+
   return (
     <Container className="my-3 mainSection">
       <Card>
@@ -8,20 +38,29 @@ const DetalleProducto = () => {
           <Col md={6}>
             <Card.Img
               variant="top"
-              src="https://images.pexels.com/photos/414555/pexels-photo-414555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+              src={producto.imagen}
+              alt={producto.nombreProducto}
             />
           </Col>
           <Col md={6}>
             <Card.Body>
-              <Card.Title className="primary-font">Capuchino</Card.Title>
+              <Card.Title className="primary-font">
+                {producto.nombreProducto}
+              </Card.Title>
               <hr />
               <Card.Text>
-              El café americano es una bebida caliente que consiste en un espresso diluido con agua caliente, lo que resulta en una taza de café suave y aromático. Es una opción popular para aquellos que prefieren un café menos intenso que el espresso tradicional. Perfecto para disfrutar en cualquier momento del día.
-              <br/>
-              <br/>
-              <span className="primary-font fw-semibold ">Categoria:</span> Infusiones
-              <br className='mb-3'/>
-              <span className="primary-font fw-semibold ">Precio: $1000</span></Card.Text>
+                {producto.descripcion_amplia}
+                <br />
+                <br />
+                <span className="primary-font fw-semibold ">
+                  Categoria:
+                </span>{" "}
+                {producto.categoria}
+                <br className="mb-3" />
+                <span className="primary-font fw-semibold ">
+                  Precio: ${producto.precio}
+                </span>
+              </Card.Text>
             </Card.Body>
           </Col>
         </Row>
